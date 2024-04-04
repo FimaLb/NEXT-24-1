c5e0cd684b66c02412559ff6aafe3c67fcdce1f2
FimaLb <amifubar@gmail.com>
2024-04-05 01:18:28 +0300

    lesson-10

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/landing",
+        destination: "/landing/en",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
